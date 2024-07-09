dad1f5d7f4ab7a9121c6136e63c7c7239872b5db
Mallazeum <kf7jrk@gmail.com>
2024-07-09 07:25:50 -0600

    prepare week 5

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,21 @@
 using System;
+using System.Formats.Asn1;
+using System.Reflection.Metadata;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(new Square("blue", 5));
+        shapes.Add(new Rectangle("green", 5, 4));
+        shapes.Add(new Circle("red", 4));
+
+        foreach (Shape shape in shapes)
+        {
+            double area = shape.GetArea();
+            string color = shape.GetColor();
+            Console.WriteLine($"{color}: {area}");
+        }
     }
 }
